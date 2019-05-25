8fa8bf0eba1ce11a6331d2f791c547fb0a2cdfd0
Dave Pearson <davep@davep.org>
2019-05-25 12:22:19 +0100

    :bookmark: v0.2

--- pinboard.el
+++ pinboard.el
@@ -2,7 +2,7 @@
 ;; Copyright 2019 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 0.1
+;; Version: 0.2
 ;; Keywords: hypermedia, bookmarking, reading, pinboard
 ;; URL: https://github.com/davep/pinboard.el
 ;; Package-Requires: ((emacs "25"))
